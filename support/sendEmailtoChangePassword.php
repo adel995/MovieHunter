d8bec1859fcd971439c4c3e5b170cfb76a123105
Adel <adbatal95@gmail.com>
2019-05-18 01:52:34 +0300

    Changed support/sendEmailtoChangePassword.php

--- support/sendEmailtoChangePassword.php
+++ support/sendEmailtoChangePassword.php
@@ -26,7 +26,7 @@
         </div>
 
         <div class="container">
-            <label for="fpass"><b>Please enter your e-mail address so we can send you the password</b></label>
+            <label for="fpass"><b>Please enter your email address so we can send you the password</b></label>
             <input type="text" placeholder="Enter your E-mail" name="email" required>
 
 
